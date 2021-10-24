51b781b0c947663e82dc3a6d60ec07dfb41b9c79
Michael Hodge <michaelhdg03@gmail.com>
2021-10-24 15:45:59 -0500

    Adds high score animation to the main menu

--- Source/ShootingGallery/HighScoreSaveGame.cpp
+++ Source/ShootingGallery/HighScoreSaveGame.cpp
@@ -8,3 +8,11 @@ UHighScoreSaveGame::UHighScoreSaveGame(){
 	SaveSlotName = TEXT("high_score");
 	UserIndex = 0;
 }
+
+// Modified function from https://mylittledevblog.wordpress.com/2018/02/15/ue4-add-leading-zeroes/
+FString UHighScoreSaveGame::GetPrettyHighScore()
+{
+	char Buffer[100];
+	snprintf(Buffer, 100, "%0*d", 7, HighScore);
+	return FString(ANSI_TO_TCHAR(Buffer));
+}
